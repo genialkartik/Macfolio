db2d252c34b215ffcdb018c70a0c36536dba615d
genialkartik <kartik9756@gmail.com>
2020-06-15 22:08:47 +0530

    completed basic structure

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var si = require('systeminformation')
+var cData = require('../api/clientinfo')
+
+var cdata = new cData()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  si.users()
+    .then(data => {
+      cdata.cDataContainer(data)
+    })
+    .catch(error => console.log(error))
   res.render('index', { title: 'Macfolio' });
 });
 
