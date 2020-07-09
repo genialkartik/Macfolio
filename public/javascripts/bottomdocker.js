ce2563a7d93dc8e388a17fdc5d288e3d7cdb5f18
genialkartik <kartik9756@gmail.com>
2020-07-10 00:24:29 +0530

    Removed v2.0 from master branch

--- public/javascripts/bottomdocker.js
+++
@@ -1,8 +0,0 @@
-// LOWER DOCKER HINT
-
-$(".dcimg").hover(function(){
-  $(this).prev().css("font-size", "0.7em");
-});
-$(".dcimg").mouseleave(function(){
-  $(this).prev().css("font-size", "0em");
-});
