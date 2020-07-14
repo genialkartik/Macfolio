023959a3d4bff679fc0934525eb4229e29f2908a
genialkartik <kartik9756@gmail.com>
2020-07-14 23:07:39 +0530

    basic UI completed

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const app = express()
 
 const visiapi = require('./routes/api/visiapi')
 
-const app = express()
-
+  
 app.use(bodyParser.json())
 
-const db = require('./config/mongoconnect').mongoURI
+const db = require('./config/keys').mongoURI
 
 mongoose.connect(db, {
     useNewUrlParser: true,
