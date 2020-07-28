d054ffe848b871053e3f0de4d91ab6fdc7bb35e1
genialkartik <kartik9756@gmail.com>
2020-07-28 21:29:31 +0530

    completed folder & file structure/functionalities

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+
 const app = express()
 
 const visiapi = require('./routes/api/visiapi')
 const explorers = require('./routes/explorer/explorer')
 
-  
+app.use(fileUpload())
 app.use(bodyParser.json())
 
 const db = require('./config/keys').mongoURI
