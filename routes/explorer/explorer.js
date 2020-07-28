30a98cbf23463fa83b26a81de79dfef67911e7f9
genialkartik <kartik9756@gmail.com>
2020-07-28 21:33:13 +0530

    file & folder structure

--- routes/explorer/explorer.js
+++ routes/explorer/explorer.js
@@ -1,49 +1,83 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
+const fs = require('fs')
 
 const Folders = require('../../models/folder')
 const Files = require('../../models/file')
-const { connection } = require('mongoose')
 
-router.route('/:wdata/:wfolder')
-.get((req, res)=>{
-    Files.find({filePath: req.params.wdata+'/'+req.params.wfolder})
-        .sort({ fileName: 1})
-        .then(items => res.json(items))
-})
-.post((req, res) => {
-    const newFile = new Files({
-        fileName: req.body.filename,
-        fileType: req.body.filetype,
-        fileSize: req.body.filesize,
-        filePath: req.body.filepath
+const staticPath = path.join(__dirname, '../../client/public/documents/')
+
+router.route('/file')
+    .post((req, res, files) => {
+        if (req.files === null) {
+            return res.status(400).json(null)
+        }
+        const newFile = new Files({
+            fileName: req.files.filetoupload.name,
+            itemType: req.files.filetoupload.mimetype,
+            fileSize: req.files.filetoupload.size,
+            filePath: req.body.itempath
+        })
+        req.files.filetoupload.mv(staticPath + req.files.filetoupload.name.replace(/\s/g, ''), err => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send(err);
+            }
+            console.log('file uploaded')
+            newFile.save()
+                .then(item => res.json(item))
+        })
     })
-    newFile.save()
-        .then(item => res.json(item))
-})
-
-router.route('/:wdata')
-.get((req, res)=>{
-    Folders.find({folderPath: req.params.wdata})
-        .sort({ folderName: 1})
-        .then(items => res.json(items))
-})
-.post((req, res) => {
-    const newFolder = new Folders({
-        folderName: req.body.fname,
-        folderSize: req.body.fsize,
-        folderPath: req.body.fpath
+    .delete((req, res) => {
+        Files.findById(req.query.delId)
+            // .then(file => file.remove().then(() => res.json({ success: true })))
+            .exec(
+                function (err, file) {
+                    if (err) {
+                        err => res.status(404).json({ success: false })
+                    }
+                    file.remove().then(() => {
+                        fs.unlink(staticPath + file.fileName.replace(/\s/g, ''), (err) => {
+                            if (err) { throw err; }
+                            res.json({ success: true })
+                        })
+                    })
+                })
     })
-    newFolder.save()
-        .then(item => res.json(item))
-})
 
-router.route('/:folderId')
-.delete((req, res)=>{
-    Folders.findById(req.params.folderId)
-        .then(folder => folder.remove().then(()=>res.json({ success: true })))
-        .catch(err=> res.status(404).json({success: false}))
-})
+router.route('/folder')
+    .get((req, res) => {
+        Folders.find({ folderPath: req.query.wpath })
+            .sort({ folderName: 1 })
+            .exec(
+                function (err, doc) {
+                    Files.find({ filePath: req.query.wpath })
+                        .sort({ fileName: 1 })
+                        .exec(
+                            function (err, doc2) {
+                                var items = [...doc, ...doc2]
+                                res.json(items)
+                            });
+                });
+    })
+    .post((req, res) => {
+        const newFolder = new Folders({
+            folderName: req.body.fname,
+            itemType: req.body.ftype,
+            folderSize: req.body.fsize,
+            folderPath: req.body.fpath
+        })
+        newFolder.save()
+            .then(item => res.json(item))
+    })
+    .delete((req, res) => {
+        console.log(req.query.delId)
+        Folders.findById(req.query.delId)
+            .then(folder => folder.remove().then(() => res.json({ success: true })))
+            .catch(err => res.status(404).json({ success: false }))
+    })
+
 
 
 module.exports = router
