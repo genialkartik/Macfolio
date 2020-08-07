8d93d7a8c42e7912e9b33b646c698fd06f5716c9
genialkartik <kartik9756@gmail.com>
2020-08-07 21:38:03 +0530

    unresolved zIndex & wPosition

--- routes/api/visiapi.js
+++ routes/api/visiapi.js
@@ -3,39 +3,44 @@ const router = express.Router()
 const sysinfo = require('systeminformation')
 
 const visiD = require('../../models/visidata')
+const { json } = require('body-parser')
 
 router.route('/')
-    .get((req, res) => {
-        var visidata = [];
-        var storeVisiData = [];
-        sysinfo.system(function (system) {
-            storeVisiData.push(system)
-        })
-        sysinfo.users(function (user) {
-            storeVisiData.push(user[0])
-        })
-        sysinfo.osInfo(function (osinfo) {
-            storeVisiData.push(osinfo)
-            visiD.find({
-                User: {
-                    user: 'tyagi'
-                }
-            })
-            .then(data=>console.log(data))
-
-            // var newVisiData = new visiD({
-            //     System: storeVisiData[0],
-            //     User: storeVisiData[1],
-            //     osInfo: storeVisiData[2]
-            // })
-            // newVisiData.save()
-            // .then(data=>console.log(data))
-        })
-        sysinfo.battery(function (btry) {
-            visidata.push(btry.ischarging)
-            visidata.push(btry.percent)
-            res.json(visidata)
-        })
+  .get((req, res) => {
+    var visidata = [];
+    var storeVisiData = [];
+    sysinfo.system(function (system) {
+      storeVisiData.push(system)
     })
+    sysinfo.users(function (user) {
+      storeVisiData.push(user[0])
+      storeVisiData.push(user[0].user)
+    })
+    sysinfo.osInfo(function (osinfo) {
+      storeVisiData.push(osinfo)
+      // Save to DB
+      var newVisiData = new visiD({
+        System: storeVisiData[0],
+        User: storeVisiData[1],
+        VisitorName: storeVisiData[2],
+        osInfo: storeVisiData[3]
+      })
+      visiD.find({ VisitorName: storeVisiData[2] })
+        .exec(
+          function (err, doc2) {
+            if (doc2.length) {
+              return 1
+            } else {
+              newVisiData.save()
+            }
+          });
+    })
+    // Goes to Client
+    sysinfo.battery(function (btry) {
+      visidata.push(btry.ischarging)
+      visidata.push(btry.percent)
+      res.json(visidata)
+    })
+  })
 
 module.exports = router;
--- routes/explorer/explorer.js
+++ routes/explorer/explorer.js
@@ -9,73 +9,78 @@ const Files = require('../../models/file')
 const staticPath = path.join(__dirname, '../../client/public/documents/')
 
 router.route('/file')
-    .post((req, res, files) => {
-        if (req.files === null) {
-            return res.status(400).json(null)
-        }
-        const newFile = new Files({
-            fileName: req.files.filetoupload.name,
-            itemType: req.files.filetoupload.mimetype,
-            fileSize: req.files.filetoupload.size,
-            filePath: req.body.itempath
-        })
-        req.files.filetoupload.mv(staticPath + req.files.filetoupload.name.replace(/\s/g, ''), err => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send(err);
-            }
-            console.log('file uploaded')
-            newFile.save()
-                .then(item => res.json(item))
-        })
+  .post((req, res, files) => {
+    if (req.files === null) {
+      return res.status(400).json(null)
+    }
+    var customFiletype = req.files.filetoupload.mimetype.toString()
+    var fileName = req.body.filenametoupload+'.'+customFiletype.substring(customFiletype.indexOf('/')+1, customFiletype.length)
+
+    
+    const newFile = new Files({
+      fileName: fileName,
+      itemType: req.files.filetoupload.mimetype,
+      fileSize: req.files.filetoupload.size,
+      filePath: req.body.itempath
     })
-    .delete((req, res) => {
-        Files.findById(req.query.delId)
-            // .then(file => file.remove().then(() => res.json({ success: true })))
-            .exec(
-                function (err, file) {
-                    if (err) {
-                        err => res.status(404).json({ success: false })
-                    }
-                    file.remove().then(() => {
-                        fs.unlink(staticPath + file.fileName.replace(/\s/g, ''), (err) => {
-                            if (err) { throw err; }
-                            res.json({ success: true })
-                        })
-                    })
-                })
+    
+    req.files.filetoupload.mv(staticPath + fileName.replace(/\s/g, ''), err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+      console.log('file uploaded')
+      newFile.save()
+        .then(item => res.json(item))
     })
+  })
+  .delete((req, res) => {
+    Files.findById(req.query.delId)
+      // .then(file => file.remove().then(() => res.json({ success: true })))
+      .exec(
+        function (err, file) {
+          if (err) {
+            err => res.status(404).json({ success: false })
+          }
+          file.remove().then(() => {
+            fs.unlink(staticPath + file.fileName.replace(/\s/g, ''), (err) => {
+              if (err) { throw err; }
+              res.json({ success: true })
+            })
+          })
+        })
+  })
 
 router.route('/folder')
-    .get((req, res) => {
-        Folders.find({ folderPath: req.query.wpath })
-            .sort({ folderName: 1 })
+  .get((req, res) => {
+    Folders.find({ folderPath: req.query.wpath })
+      .sort({ folderName: 1 })
+      .exec(
+        function (err, doc) {
+          Files.find({ filePath: req.query.wpath })
+            .sort({ fileName: 1 })
             .exec(
-                function (err, doc) {
-                    Files.find({ filePath: req.query.wpath })
-                        .sort({ fileName: 1 })
-                        .exec(
-                            function (err, doc2) {
-                                var items = [...doc, ...doc2]
-                                res.json(items)
-                            });
-                });
+              function (err, doc2) {
+                var items = [...doc, ...doc2]
+                res.json(items)
+              });
+        });
+  })
+  .post((req, res) => {
+    const newFolder = new Folders({
+      folderName: req.body.fname,
+      itemType: req.body.ftype,
+      folderSize: req.body.fsize,
+      folderPath: req.body.fpath
     })
-    .post((req, res) => {
-        const newFolder = new Folders({
-            folderName: req.body.fname,
-            itemType: req.body.ftype,
-            folderSize: req.body.fsize,
-            folderPath: req.body.fpath
-        })
-        newFolder.save()
-            .then(item => res.json(item))
-    })
-    .delete((req, res) => {
-        Folders.findById(req.query.delId)
-            .then(folder => folder.remove().then(() => res.json({ success: true })))
-            .catch(err => res.status(404).json({ success: false }))
-    })
+    newFolder.save()
+      .then(item => res.json(item))
+  })
+  .delete((req, res) => {
+    Folders.findById(req.query.delId)
+      .then(folder => folder.remove().then(() => res.json({ success: true })))
+      .catch(err => res.status(404).json({ success: false }))
+  })
 
 
 
