384f4682fbf772cd1c540b01411c9dcc0a5263aa
genialkartik <kartik9756@gmail.com>
2020-11-15 19:58:55 +0530

    s3b connected

--- routes/explorer/feedback.js
+++ routes/explorer/feedback.js
@@ -1,37 +1,46 @@
 const express = require('express')
 const rtr = express.Router()
-
+const AWS = require('aws-sdk')
 const Feedback = require('../../models/feedback')
 
+AWS.config.update({
+  accessKeyId: "",
+  secretAccessKey: ""
+});
+var s3 = new AWS.S3();
+
 rtr.route('/show')
-  .get((req, res) => {
-    Feedback.find({}, (err, doc) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(doc);
-      }
+  .get(async (req, res) => {
+    Feedback.find({}, async (err, doc) => {
+      res.json(err ? null : doc)
     })
   })
   .post(async (req, res) => {
     var r = req.body;
-    req.files.image.mv('./client/build/feedback/' + req.files.image.name, async (err) => {
-      // req.files.image.mv('./client/public/feedback/' + req.files.image.name, async (err) => {
-      if (err) {
-        console.log(err)
-        res.json(null)
-      }
-      else {
-        const newFeedback = new Feedback({
-          Name: r.name,
-          Message: r.message,
-          Star: Number(r.star),
-          ImageName: req.files.image.name
-        })
-        await newFeedback.save()
-          .then(item => res.json(item))
-      }
-    })
+    try {
+      var params = {
+        Bucket: 'BUCKETNAME',
+        Body: req.files.image.data,
+        Key: 'foldername/' + req.files.image.name.replace(/\s/g, '')
+      };
+      s3.upload(params, async (err, data) => {
+        if (err) {
+          res.json(null)
+        } else {
+          const newFeedback = new Feedback({
+            Name: r.name,
+            Message: r.message,
+            Star: Number(r.star),
+            ImageName: data.Location
+          })
+          await newFeedback.save()
+            .then(item => res.json(item))
+        }
+      })
+    } catch (error) {
+      console.log(error)
+      res.json(null)
+    }
   })
 
 module.exports = rtr
